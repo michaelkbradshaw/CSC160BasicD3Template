fb0da6ce8719b001a8d9bd99b54e09a8d4a5bc7d
michaelkbradshaw <michaelkbradshaw@users.noreply.github.com>
2021-01-22 13:20:14 -0500

    testing import

--- js/d3Test.js
+++ js/d3Test.js
@@ -1,5 +1,5 @@
-import * as d3 from "d3";
-
+//import * as d3 from "d3";
+var d3 = import("https://d3js.org/d3.v6.min.js");
 var peoplePromise = d3.csv("data/people.csv");
 
 peoplePromise.then(function (people) {

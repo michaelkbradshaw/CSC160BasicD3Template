0c76dfe7f2470685e0219dd3fbce82028d60361d
michaelkbradshaw <michaelkbradshaw@users.noreply.github.com>
2021-01-22 13:31:18 -0500

    github workaround?

--- js/d3Test.js
+++ js/d3Test.js
@@ -1,7 +1,9 @@
 //import * as d3 from "d3";
-var d3 = import("https://d3js.org/d3.v6.min.js");
-var peoplePromise = d3.csv("data/people.csv");
+var d3Promise = import("https://d3js.org/d3.v6.min.js");
+d3Promise.then(function (d3) {
+  var peoplePromise = d3.csv("data/people.csv");
 
-peoplePromise.then(function (people) {
-  console.log("data", people);
+  peoplePromise.then(function (people) {
+    console.log("data", people);
+  });
 });

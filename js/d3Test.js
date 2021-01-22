f1bbe7842588e75b17283ccce15c36114e9582f8
michaelkbradshaw <michaelkbradshaw@users.noreply.github.com>
2021-01-22 13:55:16 -0500

    d3 works.  uncomment lines to work on github pages

--- js/d3Test.js
+++ js/d3Test.js
@@ -1,7 +1,17 @@
-//import * as d3 from "d3";
-var d3 = window.d3;
+import * as d3 from "d3"; //comment for github
+// var d3 = window.d3; //uncomment for github
 var peoplePromise = d3.csv("data/people.csv");
 
 peoplePromise.then(function (people) {
   console.log("data", people);
+
+  d3.select("body")
+    .append("ol")
+    .selectAll("li")
+    .data(people)
+    .enter()
+    .append("li")
+    .text(function (person) {
+      return person.name;
+    });
 });
